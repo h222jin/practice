c02b591a11d583239f84146d911ac60fceaf0fbb
h222jin <heejiney@gmail.com>
2020-12-30 18:15:15 +0900

    2020-12-30 18:15 if

--- src/app/features/meeting-room/booking.component.ts
+++ src/app/features/meeting-room/booking.component.ts
@@ -1,10 +1,12 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, Input, OnInit, ViewEncapsulation} from "@angular/core";
 import {Booking} from "@app/features/meeting-room/booking";
 import {BookingService} from "@app/features/meeting-room/booking.service";
 
 @Component({
     selector: 'ea-booking',
-    templateUrl: './booking.component.html'
+    templateUrl: './booking.component.html',
+    styleUrls: ['../../shared/ngx-datatable-css/smartadmin-ngx-datatable.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class BookingComponent implements OnInit {
 
--- src/app/features/meeting-room/meeting-room.component.ts
+++ src/app/features/meeting-room/meeting-room.component.ts
@@ -6,49 +6,62 @@ import {Booking} from "@app/features/meeting-room/booking";
 import {BookingService} from "@app/features/meeting-room/booking.service";
 
 @Component({
-  selector: 'ea-meeting-room',
-  templateUrl: './meeting-room.component.html',
-  styleUrls: ['./meeting-room.component.css']
+    selector: 'ea-meeting-room',
+    templateUrl: './meeting-room.component.html',
+    styleUrls: ['./meeting-room.component.css']
 })
 export class MeetingRoomComponent implements OnInit {
 
-  @ViewChild('bookingTemplate') bookingTemplate;
+    @ViewChild('bookingTemplate') bookingTemplate;
 
-  @Input() public state: any;
+    @Input() public state: any;
 
-  public newBooking: Booking;
+    public newBooking: Booking;
 
-  public states: Array<any>;
-  public items: Array<Booking> = [];
+    public states: Array<any>;
+    public items: Array<Booking> = [];
 
-  constructor(
-      private bookingService: BookingService
-  ) {
-    this.states = this.bookingService.states
-  }
+    constructor(
+        private bookingService: BookingService
+    ) {
+        this.states = this.bookingService.states
+    }
 
-  ngOnInit() {
-    this.bookingService.subject.subscribe( (bookings: Array<Booking>) => {
-      console.log(bookings);
-      this.setItems(bookings);
-    })
+    ngOnInit() {
+        this.bookingService.subject.subscribe((bookings: Array<Booking>) => {
+            console.log(bookings);
+            this.setItems(bookings);
+        })
 
-    this.setItems(this.bookingService.bookings);
-  }
+        this.setItems(this.bookingService.bookings);
+    }
 
-  setItems(bookings: Array<Booking>) {
-    this.items = bookings.filter(it => it.state == this.state.name);
-  }
+    setItems(bookings: Array<Booking>) {
+        this.items = bookings.filter(it => it.state == this.state);
+    }
 
 
-  createBooking() {
-    this.bookingService.createBooking(this.newBooking);
-    this.newBooking = null;
-  }
+    createBooking() {
+        if (this.newBooking.hours != null
+            && this.newBooking.state != 'booked'
+            && this.newBooking.startDate != null
+            && this.newBooking.endDate != null
+            && this.newBooking.leader != null
+            && this.newBooking.roomName != null
+            && this.newBooking.agenda != null) {
+            this.bookingService.createBooking(this.newBooking);
+            this.newBooking = null;
+        } else {
+            alert('정확히 입력해 주세요')
+        }
 
-  booking() {
-    // new booking 으로 init
-    this.newBooking = new Booking();
-    this.newBooking.state = 'available';
-  }
+    }
+
+    booking() {
+        // new booking 으로 init
+        this.newBooking = new Booking();
+        this.newBooking.state = 'available';
+    }
+
+
 }
